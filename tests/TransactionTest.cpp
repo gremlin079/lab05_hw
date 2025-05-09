0874f2007e2e40233afaf6f6d98f2b84883287b1
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 13:54:26 +0300

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -3,28 +3,34 @@
 #include "banking/Transaction.h"
 #include "banking/Account.h"
 
-class TestableTransaction : public Transaction {
+class TransactionWrapper : public Transaction {
 public:
-    using Transaction::SaveToDataBase;
+    void TestableSaveToDataBase(Account& from, Account& to, int sum) {
+        SaveToDataBase(from, to, sum);
+    }
 };
 
-class MockTransaction : public TestableTransaction {
+class MockTransaction : public TransactionWrapper {
 public:
-    MOCK_METHOD(void, SaveToDataBase, (Account&, Account&, int), (override));
-};
+    MOCK_METHOD(void, TestableSaveToDataBase, (Account&, Account&, int));
 
-TEST(TransactionTest, MakeThrowsIfSameAccount) {
-    MockTransaction trans;
-    Account acc1(1, 1000), acc2(1, 1000);
-    EXPECT_THROW(trans.Make(acc1, acc2, 100), std::logic_error);
-}
+    void SaveToDataBase(Account& from, Account& to, int sum) override {
+        TestableSaveToDataBase(from, to, sum);
+    }
+};
 
 TEST(TransactionTest, MakeCallsSaveToDataBase) {
     MockTransaction trans;
     Account acc1(1, 1000), acc2(2, 1000);
     
-    EXPECT_CALL(trans, SaveToDataBase(testing::Ref(acc1), testing::Ref(acc2), 200))
+    EXPECT_CALL(trans, TestableSaveToDataBase(testing::Ref(acc1), testing::Ref(acc2), 200))
         .Times(1);
         
     trans.Make(acc1, acc2, 200);
+}
+
+TEST(TransactionTest, MakeThrowsIfSameAccount) {
+    MockTransaction trans;
+    Account acc1(1, 1000), acc2(1, 1000);
+    EXPECT_THROW(trans.Make(acc1, acc2, 100), std::logic_error);
 }
