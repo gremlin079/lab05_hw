706246a4b4b25d29f064c5e36bc000b30dcac8b0
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 13:52:02 +0300

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -1,27 +1,30 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "banking/Transaction.h"
 #include "banking/Account.h"
 
-class MockTransaction : public Transaction {
+class TestableTransaction : public Transaction {
+public:
+    using Transaction::SaveToDataBase;
+};
+
+class MockTransaction : public TestableTransaction {
 public:
     MOCK_METHOD(void, SaveToDataBase, (Account&, Account&, int), (override));
 };
 
 TEST(TransactionTest, MakeThrowsIfSameAccount) {
+    MockTransaction trans;
     Account acc1(1, 1000), acc2(1, 1000);
-    Transaction t;
-    EXPECT_THROW(t.Make(acc1, acc2, 100), std::logic_error);
+    EXPECT_THROW(trans.Make(acc1, acc2, 100), std::logic_error);
 }
 
-TEST(TransactionTest, MakeThrowsIfSumNegative) {
+TEST(TransactionTest, MakeCallsSaveToDataBase) {
+    MockTransaction trans;
     Account acc1(1, 1000), acc2(2, 1000);
-    Transaction t;
-    EXPECT_THROW(t.Make(acc1, acc2, -50), std::invalid_argument);
-}
-
-TEST(TransactionTest, MakeFailsIfFeeTooHigh) {
-    Account acc1(1, 1000), acc2(2, 1000);
-    Transaction t;
-    t.set_fee(60);  // fee > sum / 2
-    EXPECT_FALSE(t.Make(acc1, acc2, 100));
+    
+    EXPECT_CALL(trans, SaveToDataBase(testing::Ref(acc1), testing::Ref(acc2), 200))
+        .Times(1);
+        
+    trans.Make(acc1, acc2, 200);
 }
