87bf52ce1ca7b944a4863a6a62eb53758346b0de
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 13:48:40 +0300

    Update AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -1,9 +1,11 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include "banking/Account.h"
 
 class MockAccount : public Account {
 public:
     MockAccount(int id, int balance) : Account(id, balance) {}
+
     MOCK_METHOD(int, GetBalance, (), (const, override));
     MOCK_METHOD(void, ChangeBalance, (int), (override));
     MOCK_METHOD(void, Lock, (), (override));
