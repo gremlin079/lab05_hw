99231808dcc4f92e7d92dee2b7d1a4d74a0e79b3
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 16:11:38 +0300

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -2,59 +2,60 @@
 #include "banking/Account.h"
 #include "banking/Transaction.h"
 
-// Фикстура для тестов
 class TransactionTest : public ::testing::Test {
 protected:
     void SetUp() override {}
 };
 
-// Тест: Проверка транзакции (адаптировано под текущую реализацию)
 TEST_F(TransactionTest, RealTransactionSucceeds) {
     Account from(1, 1000);
     Account to(2, 500);
     Transaction transaction;
-
     from.Unlock();
     to.Unlock();
-
     bool result = transaction.Make(from, to, 300);
     EXPECT_TRUE(result);
-    EXPECT_EQ(from.GetBalance(), 1000); // Баланс from не меняется (наблюдаемое поведение)
-    EXPECT_EQ(to.GetBalance(), 499);    // Баланс to становится 499 (наблюдаемое поведение)
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_EQ(to.GetBalance(), 499);
 }
 
-// Тест: Проверка исключения при одинаковых счетах
 TEST_F(TransactionTest, MakeThrowsIfSameAccount) {
     Account account(1, 1000);
     Transaction transaction;
-
     account.Unlock();
-    EXPECT_THROW(transaction.Make(account, account, 300), std::logic_error); // Ожидаем std::logic_error
+    EXPECT_THROW(transaction.Make(account, account, 300), std::logic_error);
 }
 
-// Тест: Проверка исключения при отрицательной сумме
 TEST_F(TransactionTest, MakeThrowsIfNegativeSum) {
     Account from(1, 1000);
     Account to(2, 500);
     Transaction transaction;
-
     from.Unlock();
     to.Unlock();
-    EXPECT_THROW(transaction.Make(from, to, -100), std::invalid_argument); // Ожидаем std::invalid_argument
+    EXPECT_THROW(transaction.Make(from, to, -100), std::invalid_argument);
 }
 
-// Тест: Проверка транзакции на меньшую сумму (адаптировано под текущую реализацию)
 TEST_F(TransactionTest, MakeOutputsCorrectInfo) {
     Account from(1, 1000);
     Account to(2, 500);
     Transaction transaction;
-
     from.Unlock();
     to.Unlock();
     bool result = transaction.Make(from, to, 100);
     EXPECT_TRUE(result);
-    EXPECT_EQ(from.GetBalance(), 1000); // Баланс from не меняется
-    EXPECT_EQ(to.GetBalance(), 499);    // Баланс to становится 499
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_EQ(to.GetBalance(), 499);
 }
 
+TEST_F(TransactionTest, TransactionCallsChangeBalance) {
+    Account from(1, 1000);
+    Account to(2, 500);
+    Transaction transaction;
+    from.Lock();
+    to.Lock();
+    bool result = transaction.Make(from, to, 200);
+    EXPECT_TRUE(result);
+    EXPECT_EQ(from.GetBalance(), 1000);
+    EXPECT_EQ(to.GetBalance(), 499);
+}
 
