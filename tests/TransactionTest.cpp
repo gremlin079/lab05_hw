ef5f3956541d5b7148a81ed5d9e86e8de3a0bccc
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 13:24:35 +0300

    Create TransactionTest.cpp

---
+++ tests/TransactionTest.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "../Transaction.h"
+#include "../Account.h"
+
+class MockTransaction : public Transaction {
+public:
+    MOCK_METHOD(void, SaveToDataBase, (Account&, Account&, int), (override));
+};
+
+TEST(TransactionTest, MakeThrowsIfSameAccount) {
+    Account acc1(1, 1000), acc2(1, 1000);
+    Transaction t;
+    EXPECT_THROW(t.Make(acc1, acc2, 100), std::logic_error);
+}
+
+TEST(TransactionTest, MakeThrowsIfSumNegative) {
+    Account acc1(1, 1000), acc2(2, 1000);
+    Transaction t;
+    EXPECT_THROW(t.Make(acc1, acc2, -50), std::invalid_argument);
+}
+
+TEST(TransactionTest, MakeFailsIfFeeTooHigh) {
+    Account acc1(1, 1000), acc2(2, 1000);
+    Transaction t;
+    t.set_fee(60);  // fee > sum / 2
+    EXPECT_FALSE(t.Make(acc1, acc2, 100));
+}
