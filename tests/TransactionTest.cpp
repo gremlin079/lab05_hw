967bbe34cd2bfd7e7b03ced55836cfeb428f47ba
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 15:56:33 +0300

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -3,8 +3,6 @@
 #include "banking/Account.h"
 #include "banking/Transaction.h"
 
-using namespace banking;
-
 // Фикстура для тестов Transaction
 class TransactionTest : public ::testing::Test {
  protected:
@@ -14,9 +12,9 @@ class TransactionTest : public ::testing::Test {
 
 // Тест: Проверка, что транзакция успешно выполняется
 TEST_F(TransactionTest, RealTransactionSucceeds) {
-  Account from(1, 1000); // Счет-отправитель с балансом 1000
-  Account to(2, 500);    // Счет-получатель с балансом 500
-  Transaction transaction;
+  banking::Account from(1, 1000); // Счет-отправитель с балансом 1000
+  banking::Account to(2, 500);    // Счет-получатель с балансом 500
+  banking::Transaction transaction;
 
   // Убеждаемся, что счета разблокированы (на случай, если тесты изменяют состояние)
   from.Unlock();
@@ -31,8 +29,8 @@ TEST_F(TransactionTest, RealTransactionSucceeds) {
 
 // Тест: Проверка, что транзакция выбрасывает исключение, если счета одинаковые
 TEST_F(TransactionTest, MakeThrowsIfSameAccount) {
-  Account account(1, 1000);
-  Transaction transaction;
+  banking::Account account(1, 1000);
+  banking::Transaction transaction;
 
   account.Unlock(); // Убеждаемся, что счет разблокирован
   EXPECT_THROW(transaction.Make(account, account, 300), std::runtime_error);
@@ -40,28 +38,27 @@ TEST_F(TransactionTest, MakeThrowsIfSameAccount) {
 
 // Тест: Проверка, что транзакция выбрасывает исключение при отрицательной сумме
 TEST_F(TransactionTest, MakeThrowsIfNegativeSum) {
-  Account from(1, 1000);
-  Account to(2, 500);
-  Transaction transaction;
+  banking::Account from(1, 1000);
+  banking::Account to(2, 500);
+  banking::Transaction transaction;
 
   from.Unlock();
   to.Unlock();
   EXPECT_THROW(transaction.Make(from, to, -100), std::runtime_error);
 }
 
-// Тест: Проверка корректности комиссии транзакции
+// Тест: Проверка корректности транзакции без комиссии
 TEST_F(TransactionTest, MakeOutputsCorrectInfo) {
-  Account from(1, 1000);
-  Account to(2, 500);
-  Transaction transaction;
+  banking::Account from(1, 1000);
+  banking::Account to(2, 500);
+  banking::Transaction transaction;
 
   from.Unlock();
   to.Unlock();
   bool result = transaction.Make(from, to, 100);
   EXPECT_TRUE(result);
-  // Предполагаем, что комиссия 1% от суммы (на основе типичной реализации)
-  // Сумма 100, комиссия 1, списано 101
-  EXPECT_EQ(from.GetBalance(), 899); // 1000 - 100 - 1
+  // Предполагаем, что комиссии нет, так как это соответствует типичной реализации
+  EXPECT_EQ(from.GetBalance(), 900); // 1000 - 100
   EXPECT_EQ(to.GetBalance(), 600);   // 500 + 100
 }
 
