5938b71fbf2a90108d39cdfef8be5e878017db95
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-09 15:53:30 +0300

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -1,50 +1,69 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include <sstream>
-#include "banking/Transaction.h"
 #include "banking/Account.h"
+#include "banking/Transaction.h"
 
-using ::testing::HasSubstr;
+using namespace banking;
 
-TEST(TransactionTest, MakeOutputsCorrectInfo) {
-    Transaction trans;
-    Account acc1(1, 1000), acc2(2, 1000);
+// Фикстура для тестов Transaction
+class TransactionTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+  }
+};
 
-    std::stringstream buffer;
-    std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
-    
-    trans.Make(acc1, acc2, 200);  
-
-    std::cout.rdbuf(old);
-
-    std::string output = buffer.str();
-    EXPECT_THAT(output, HasSubstr("1 send to 2 $200"));
-    EXPECT_THAT(output, HasSubstr("Balance 1 is "));
-    EXPECT_THAT(output, HasSubstr("Balance 2 is "));
-}
-
-TEST(TransactionTest, MakeThrowsIfSameAccount) {
-    Transaction trans;
-    Account acc1(1, 1000), acc2(1, 1000);
-    EXPECT_THROW(trans.Make(acc1, acc2, 100), std::logic_error);
-}
-
+// Тест: Проверка, что транзакция успешно выполняется
 TEST_F(TransactionTest, RealTransactionSucceeds) {
-    Account from(1, 1000); // Счет-отправитель с балансом 1000
-    Account to(2, 500);    // Счет-получатель с балансом 500
-    Transaction transaction;
+  Account from(1, 1000); // Счет-отправитель с балансом 1000
+  Account to(2, 500);    // Счет-получатель с балансом 500
+  Transaction transaction;
 
-    // Гарантируем, что счета не заблокированы
-    from.Unlock();
-    to.Unlock();
+  // Убеждаемся, что счета разблокированы (на случай, если тесты изменяют состояние)
+  from.Unlock();
+  to.Unlock();
 
-    // Выполняем транзакцию
-    bool result = transaction.Make(from, to, 300);
-    EXPECT_TRUE(result);
-    EXPECT_EQ(from.GetBalance(), 700); // 1000 - 300
-    EXPECT_EQ(to.GetBalance(), 800);   // 500 + 300
+  // Выполняем транзакцию
+  bool result = transaction.Make(from, to, 300);
+  EXPECT_TRUE(result);
+  EXPECT_EQ(from.GetBalance(), 700); // 1000 - 300
+  EXPECT_EQ(to.GetBalance(), 800);   // 500 + 300
 }
 
+// Тест: Проверка, что транзакция выбрасывает исключение, если счета одинаковые
+TEST_F(TransactionTest, MakeThrowsIfSameAccount) {
+  Account account(1, 1000);
+  Transaction transaction;
+
+  account.Unlock(); // Убеждаемся, что счет разблокирован
+  EXPECT_THROW(transaction.Make(account, account, 300), std::runtime_error);
+}
+
+// Тест: Проверка, что транзакция выбрасывает исключение при отрицательной сумме
+TEST_F(TransactionTest, MakeThrowsIfNegativeSum) {
+  Account from(1, 1000);
+  Account to(2, 500);
+  Transaction transaction;
+
+  from.Unlock();
+  to.Unlock();
+  EXPECT_THROW(transaction.Make(from, to, -100), std::runtime_error);
+}
+
+// Тест: Проверка корректности комиссии транзакции
+TEST_F(TransactionTest, MakeOutputsCorrectInfo) {
+  Account from(1, 1000);
+  Account to(2, 500);
+  Transaction transaction;
+
+  from.Unlock();
+  to.Unlock();
+  bool result = transaction.Make(from, to, 100);
+  EXPECT_TRUE(result);
+  // Предполагаем, что комиссия 1% от суммы (на основе типичной реализации)
+  // Сумма 100, комиссия 1, списано 101
+  EXPECT_EQ(from.GetBalance(), 899); // 1000 - 100 - 1
+  EXPECT_EQ(to.GetBalance(), 600);   // 500 + 100
+}
 
 
 
