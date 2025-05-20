525bec6990a6268d54a12e869ec199b1336e1f35
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-20 15:09:46 +0300

    Update AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -12,6 +12,11 @@ public:
     MOCK_METHOD(void, Unlock, (), (override));
 };
 
+TEST(AccountTest, IdMethod) {
+    Account acc(42, 1000);
+    EXPECT_EQ(acc.id(), 42);
+}
+
 TEST(AccountTest, LockUnlock) {
     MockAccount acc(1, 1000);
     EXPECT_CALL(acc, Lock()).Times(1);
@@ -20,9 +25,24 @@ TEST(AccountTest, LockUnlock) {
     acc.Unlock();
 }
 
+TEST(AccountTest, LockThrowsIfAlreadyLocked) {
+    Account acc(1, 1000);
+    acc.Lock();
+    EXPECT_THROW(acc.Lock(), std::runtime_error);
+    acc.Unlock();
+}
+
 TEST(AccountTest, ChangeBalanceFailsIfNotLocked) {
     Account acc(1, 1000);
     EXPECT_THROW(acc.ChangeBalance(100), std::runtime_error);
+}
+
+TEST(AccountTest, ChangeBalanceWithNegativeDiff) {
+    Account acc(1, 1000);
+    acc.Lock();
+    acc.ChangeBalance(-100);
+    acc.Unlock();
+    EXPECT_EQ(acc.GetBalance(), 900);
 }
 
 TEST(AccountTest, LockUnlockAndChangeBalance) {
